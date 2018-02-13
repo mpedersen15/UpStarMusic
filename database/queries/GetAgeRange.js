d55f7e737825865b19d65a067c5cacb442ea585f
Matt Pedersen <mpedersen@mindrocketdesign.com>
2018-02-12 18:32:11 -0700

    wired up create, delete, edit, getAgeRange and getYearsActiveRange

--- database/queries/GetAgeRange.js
+++ database/queries/GetAgeRange.js
@@ -6,4 +6,14 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+
+    return Promise.all([
+        Artist.find({}).sort({ age: 1 }).limit(1).then(artists => artists[0].age),
+        Artist.find({}).sort({ age: -1 }).limit(1).then(artists => artists[0].age)
+    ]).then(values => {
+        return {
+            min: values[0],
+            max: values[1]
+        };
+    });
 };
--- database/queries/GetYearsActiveRange.js
+++ database/queries/GetYearsActiveRange.js
@@ -6,4 +6,13 @@ const Artist = require('../models/artist');
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
 module.exports = () => {
+    return Promise.all([
+        Artist.find({}).sort({ yearsActive: 1 }).limit(1).then(artists => artists[0].yearsActive),
+        Artist.find({}).sort({ yearsActive: -1 }).limit(1).then(artists => artists[0].yearsActive)
+    ]).then(values => {
+        return {
+            min: values[0],
+            max: values[1]
+        };
+    });
 };
