ae5925969c9ec45244d5b81432c1d8036c0c8ecb
mpedersen15 <mpedersen@mindrocketdesign.com>
2018-02-13 10:57:20 -0700

    added sorting, offset and limit to searchArtist query

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -9,4 +9,14 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+    return Artist.find({}).sort({ [sortProperty]: 1 }).skip(offset).limit(limit)
+        .then(users => {
+            return {
+                all: users,
+                count: users.length,
+                offset,
+                limit
+            };
+        });
+
 };
